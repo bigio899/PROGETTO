e4ed40c4367c7ac183f6b6b4a528e857efa16b81
pietromusci <110541993+pietromusci@users.noreply.github.com>
2023-05-22 18:17:12 +0200

    Ads Integration Plugins Deleted

--- madonn/Assets/Scripts/Advertisment/AdsInitializer.cs
+++ madonn/Assets/Scripts/Advertisment/AdsInitializer.cs
@@ -6,7 +6,7 @@ public class AdsInitializer : MonoBehaviour, IUnityAdsInitializationListener
 {
     [SerializeField] string _androidGameId;
     [SerializeField] string _iOSGameId;
-    public bool _testMode = true;
+    private bool _testMode = false;
     private string _gameId;
 
     public RewardedAdsButton rewardedAdsButton; //reference of rewarded button script.
@@ -27,11 +27,11 @@ public class AdsInitializer : MonoBehaviour, IUnityAdsInitializationListener
     public void InitializeAds()
     {
 #if UNITY_IOS
-            _gameId = _iOSGameId; 
+            _gameId = _iOSGameId;
 #elif UNITY_ANDROID
         _gameId = _androidGameId;
 #elif UNITY_EDITOR
-            _gameId = _androidGameId; //Only for testing the functionality in the Editor
+            _gameId = _androidGameId ; //Only for testing the functionality in the Editor
 #endif
         if (!Advertisement.isInitialized && (Advertisement.isSupported))
         {
